01b0671d774c8d0cb9b7b7d682a00223263a5ab1
Riviera <root@zazu.riviera.local>
2022-09-09 18:30:47 -0700

    Update dockerbuild.sh to rerun container

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,1 +1,16 @@
-docker build -t firefly --platform linux/amd64 --build-arg build_base=apache --build-arg build_platform=8.0 .
+PORT=4049
+CONTAINER=firefly-dev
+docker stop $(docker ps -aq --filter "name=$CONTAINER")
+docker rm $(docker ps -aq --filter "name=$CONTAINER")
+docker build -t $CONTAINER --platform linux/amd64 --build-arg build_base=apache --build-arg build_platform=8.0 .
+docker run -d \
+-v /volume1/docker/firefly/storage/upload:/var/www/firefly-iii/storage/upload \
+-p $PORT:8080 \
+-e APP_KEY=BKXegT7KYqZxgBrDtobceJwIGj5iFuti \
+-e DB_HOST=192.168.0.70:3306 \
+-e DB_PORT=3306 \
+-e DB_CONNECTION=mysql \
+-e DB_DATABASE=firefly \
+-e DB_USERNAME=root \
+-e DB_PASSWORD=h0meserver_DB \
+$CONTAINER:latest
